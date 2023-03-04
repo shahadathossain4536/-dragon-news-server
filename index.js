1628dcdbb33a3162d83e16266ae3f6754549a4d3
Shahadat Hossain <60577101+shahadathossain4536@users.noreply.github.com>
2023-03-04 07:36:34 +0600

    ...

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-
+const cors = require("cors");
 const categories = require("./data/categories.json");
-
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
